c7002c38df669b4f4a49613a9a4f7f9e7528ab23
Shankar Manjunatha <shman@amazon.com>
2018-08-30 00:36:32 -0700

    Interpret output array. Separate model creation from deployment

--- code_folder/model_creation.py
+++ code_folder/model_creation.py
@@ -4,51 +4,34 @@ import tensorflow as tf
 import numpy as np
 import pickle
 from tensorflow.keras.models import save_model, load_model
-#from matplotlib import pyplot as plt
 import cv2
 
+# Load Data
 mnist = tf.keras.datasets.mnist
-
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
-image_0 = x_test[0]
-print image_0
-image = np.asarray(image_0, dtype="uint8")
-cv2.imwrite('image_gray.png',image)
+
+for idx in range(0,10):
+    image = np.asarray(x_test[idx], dtype="uint8")
+    cv2.imwrite('image_gray'+str(idx)+'.png',image)
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
+model.fit(x_train, y_train, epochs=5)
 
+#Save:
+save_model(model, 'digit_model.h5')
+del model # delete
 
-#plt.imshow(images[0].reshape(28, 28), cmap=plt.cm.binary)
-#plt.show()
-
-# model = tf.keras.models.Sequential([
-#   tf.keras.layers.Flatten(),
-#   tf.keras.layers.Dense(512, activation=tf.nn.relu),
-#   tf.keras.layers.Dropout(0.2),
-#   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-# ])
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-
-# #TODO : Make this 5
-# model.fit(x_train, y_train, epochs=5)
-
-# #Save:
-# save_model(model, 'digit_model.h5')
-# del model # delete
-
-# digit_model = load_model('digit_model.h5')
-# #digit_model.evaluate(x_test, y_test)
-
-
-
-# output_digits = digit_model.predict(images)
-# print output_digits
-# #model.predict()
-
-# # Notes:
-# # 1. Installed packages : virtualenv, tensorflow, h5py (for saving models)
+# Notes:
+# 1. Installed packages : virtualenv, tensorflow, h5py (for saving models)
 
--- code_folder/model_deployment.py
+++ code_folder/model_deployment.py
@@ -1,11 +1,26 @@
 from tensorflow.keras.models import load_model
 import tensorflow as tf
+import numpy as np
 
 mnist = tf.keras.datasets.mnist
 
+(_, _),(x_test, y_test) = mnist.load_data()
+
+TEST_DIM = 10
+images = x_test[0:TEST_DIM]
+
+def interpret_output_array(output_array, array_size):
+    predicted_list = []
+    THRESHOLD_VALUE = 0.8 # Sample threshold to prevent non-digits from being recognized as digits
+    for idx in range (0,array_size):
+        if( np.max(output_array[idx]) > THRESHOLD_VALUE):
+            predicted_list.append(np.argmax(output_array[idx]))
+        else:
+            predicted_list += -1 #'UNRECOGNIZED_SYMBOL' 
+    return predicted_list
+
 digit_model = load_model('digit_model.h5')
-images = mnist.test.images[0:10]
+output_array = digit_model.predict(images, TEST_DIM)
+print interpret_output_array(output_array, TEST_DIM)
 
-
-output_digits = digit_model.predict(images)
-print output_digits
+print 'expected_list' + str (y_test[0:TEST_DIM])
